c5fe875b954f3220c3e2b49eb92ea5e38e5c5b35
Yasmin Maria Zerbielli <113360477+yasminzerbielli@users.noreply.github.com>
2023-05-15 10:47:58 -0300

    Add files via upload

---
+++ trab.js
@@ -0,0 +1,11 @@
+function armazenar(){
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var area = document.getElementById("area").value;
+    var siape = document.getElementById("siape").value;
+    var carga = document.getElementById("carga").value;
+    console.log(nome)
+}
+
+//! localStorage.setItem("nome", nome)
+
